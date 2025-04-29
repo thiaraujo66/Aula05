46b6fa670c4ebf891b8548aa785c0a1fa99e1281
thiaraujo66 <thiagi-araujo@hotmail.com.br>
2025-04-29 19:24:04 -0300

    Adicionar arquivos de projeto.

---
+++ Aula05/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Aula05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aula05.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
